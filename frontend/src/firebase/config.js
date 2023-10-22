672dc1ff49bd1b1064e691ec7da46256ebdf4ec4
Горан Китић <kiticgoran90@gmail.com>
2023-10-22 13:59:55 +0200

    Update config.js

--- frontend/src/firebase/config.js
+++ frontend/src/firebase/config.js
@@ -3,12 +3,7 @@ import { getStorage } from "firebase/storage";
 
 // Web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAqrDtdx0l0XXPH2QJ6-q4a17dv-fmeKso",
-  authDomain: "harbour-860f9.firebaseapp.com",
-  projectId: "harbour-860f9",
-  storageBucket: "harbour-860f9.appspot.com",
-  messagingSenderId: "113546249057",
-  appId: "1:113546249057:web:d682a013bba7e6bcd0bb20"
+  // Firebase config here
 };
 
 // Initialize Firebase
