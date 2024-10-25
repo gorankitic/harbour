921cd4a7dc9f423bc68278f6a148bf07291523a0
Горан Китић <kiticgoran90@gmail.com>
2024-10-26 01:38:08 +0200

    Update likeRoutes.js

--- backend/routes/likeRoutes.js
+++ backend/routes/likeRoutes.js
@@ -2,13 +2,11 @@ const express = require('express');
 const { protect } = require('../controllers/authController');
 const { like, getLikes, unlike } = require('../controllers/likeController');
 
-
 const router = express.Router({ mergeParams: true });
 
 router.route('/')
     .post(protect, like)
     .get(protect, getLikes)
     .delete(protect, unlike);
-
 
 module.exports = router;
